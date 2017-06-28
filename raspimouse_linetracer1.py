ce684d25b76587b7b50f365041a2e3f9bdd7dbd1
Tiryoh <tiryoh@gmail.com>
2017-06-28 15:26:23 +0900

    Add raspimouse_linetracer1.py

---
+++ raspimouse_linetracer1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import time
+
+def getLineSensorData():
+    with open('/dev/rtlightsensor0','r') as f:
+        return map(int, f.readline().split())
+
+if __name__ == '__main__':
+    while 1: #無限ループ
+        left_end, left, right, right_end = getLineSensorData()
+        print(left, right)
+        diff = left - right #内側の2つのセンサ値の差分を取得
+        print(diff)
+        time.sleep(0.05)
+
