f9071d5aea828275fefed0b005736f56181a582e
Tiryoh <tiryoh@gmail.com>
2017-08-03 10:38:44 +0900

    Refactoring

--- raspimouse_linefollower4.py
+++ raspimouse_linefollower4.py
@@ -23,15 +23,15 @@ def get_switch_input(swnum):
     with open('/dev/rtswitch'+str(swnum),'r') as f:
         return not(int(f.readline()))
 
-# センサ値の値域が大きく異なる場合に補正するための関数を用意
-def correct_seneor_value(current, old_min, old_max):
-    return int((current - old_min) / float(old_max - old_min) * 100)
+# センサ値を正規化するための関数を用意
+def normalize(v, rng):
+    return (v - rng[0]) / (rng[1] - rng[0])
 
-# センサの個体差補正用の変数宣言
-left_end_min, left_end_max = 240, 640
-left_min, left_max = 650, 1350
-right_min, right_max = 550, 1400
-right_end_min, right_end_max = 500, 1600
+# センサの値の範囲
+left_end_range  = (240, 640)
+left_range      = (650, 1350)
+right_range     = (550, 1400)
+right_end_range = (500, 1600)
 
 if __name__ == '__main__':
     while not get_switch_input(0): # SW0が押されるまで待機
@@ -41,13 +41,13 @@ if __name__ == '__main__':
     while not get_switch_input(1): # SW1が押されたら止める
         # センサ値を取得
         left_end, left, right, right_end = get_line_sensor_data()
-        # 内側の2つセンサ値を補正・比較
-        diff1 = correct_seneor_value(left, left_min, left_max) - correct_seneor_value(right, right_min, right_max)
-        # 外側の2つセンサ値を補正・比較
-        diff2 = correct_seneor_value(left_end, left_end_min, left_end_max) - correct_seneor_value(right_end, right_end_min, right_end_max)
-        # 補正したセンサ値を合算
-        diff = diff1 / 2 + diff2
-        print(diff1, diff2)
-        set_motor_speed(100+diff, 100-diff)
+        # 内側の2つセンサ値を校正・比較
+        diff_in = normalize(left, left_range) - normalize(right, right_range)
+        # 外側の2つセンサ値を校正・比較
+        diff_out = normalize(left_end, left_end_range) - normalize(right_end, right_end_range)
+        # 校正したセンサ値を合算
+        diff = diff_in/2 + diff_out
+        print(diff_in, diff_out)
+        set_motor_speed(100*(1+diff), 100*(1-diff))
         time.sleep(0.1) # 0.1秒毎にセンサ値とモータの回転角度を更新
     set_motor_power(False)
