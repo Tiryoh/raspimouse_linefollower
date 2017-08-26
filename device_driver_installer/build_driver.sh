c864fdea6d6fd9b58f7ad4567ad23dc079b451fe
Tiryoh <tiryoh@gmail.com>
2017-08-26 23:35:27 +0900

    Add license notation

--- device_driver_installer/build_driver.sh
+++ device_driver_installer/build_driver.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -eu
 
+# MIT License (C) 2017 Tiryoh<tiryoh@gmail.com>
+# https://tiryoh.mit-license.org
+
 [ -e /usr/src/linux-headers-`uname -r` ] || { sudo apt-get update; sudo apt-get install -y raspberrypi-kernel-headers; }
 [ -e $HOME/RaspberryPiMouse ] && { cd $HOME/RaspberryPiMouse; git pull; } || { cd $HOME; git clone https://github.com/rt-net/RaspberryPiMouse.git; }
 cd $HOME/RaspberryPiMouse/src/drivers
--- device_driver_installer/install_driver.sh
+++ device_driver_installer/install_driver.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -eu
 
+# MIT License (C) 2017 Tiryoh<tiryoh@gmail.com>
+# https://tiryoh.mit-license.org
+
 [ -z $(uname -m | grep armv7l) ] && { echo "Run this script on Raspberry Pi"; exit 0; }
 
 if [ -e $HOME/RaspberryPiMouse/src/drivers/rtmouse.ko ]; then
